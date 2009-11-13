196bd25af3b66b5fdb599721c76f640615184a7d
Ivan Acosta-Rubio <ivan@bakedweb.net>
2009-11-13 12:06:55 -0500

    adding wufoo

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
 require 'sinatra'
+require 'yaml'
+require 'wufoo'
+
+# CONFIGURATION FILE FOR WUFOO
+config = YAML::load(File.read('config/wufoo'))
 
 
 helpers do
@@ -11,6 +16,40 @@ end
 
 get '/' do
   haml :layout
+end
+
+
+post '/wufoo' do 
+  
+  client = Wufoo::Client.new(config['wufoo_url'], config['api_key'])
+  submission = Wufoo::Submission.new(client, config['form_name'])
+
+  response = submission.add_params({
+    # ADD HERE FORM PARAMETERS. GO TO FORM API for VALUES
+    '7'  => params[:nombre],
+  }).process
+
+
+  if response.success?
+    
+    #@mensaje = response.message
+    @message = "Gracias por contactarnos. Te llamaremos dentro de 2 dias abiles o menos"
+  else
+    # Something was wrong with the request
+    # (missing api key, invalid form, etc)
+    if response.fail?
+     @mensaje = response.error    
+    end
+
+    # validation errors
+    unless response.valid?
+      errors = response.errors.collect { |e| "#{e.field_id} (#{e.code}): #{e.message}" }    
+      @mensaje = errors * "\n"
+    end
+  end
+  
+  haml :wufoo
+  
 end
 
 
